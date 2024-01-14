884b3a961751a0ed9e86b4fc808ef59130a004d2
Matteo-tech <matteo.guglielmi.ma.gu@gmail.com>
2024-01-14 21:47:59 +0100

    feat: add LSP autocommand
    When and LSP attaches to a buffer, custom key maps become available.

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -1,22 +1,22 @@
+-- autocommand to set formatoptions :  a sequence of letters which describes how automatic formatting is to be done.
+-- in particular:
+--  c: Auto-wrap comments using textwidth
+--  r: Automatically insert the current comment leader after hitting <Enter> in Insert mode.
+--  o: Automatically insert the current comment leader after hitting 'o' or 'O' in Normal mode.
 vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
   callback = function()
     vim.cmd "set formatoptions-=cro"
   end,
 })
 
+-- this command ensures that when opening a buffer of a specific file type, the latter is silently closed and not added to the buffer list
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = {
-    "netrw",
-    "Jaq",
-    "qf",
     "git",
     "help",
     "man",
     "lspinfo",
-    "spectre_panel",
-    "lir",
-    "DressingSelect",
-    "tsplayground",
+    "nvim",
     "",
   },
   callback = function()
@@ -27,12 +27,14 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
   end,
 })
 
+-- when entering a terminal, this command ensures that we enter normal mode
 vim.api.nvim_create_autocmd({ "CmdWinEnter" }, {
   callback = function()
     vim.cmd "quit"
   end,
 })
 
+-- when splittings, this command ensures that the window sizes are equal
 vim.api.nvim_create_autocmd({ "VimResized" }, {
   callback = function()
     vim.cmd "tabdo wincmd ="
@@ -46,16 +48,44 @@ vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
   end,
 })
 
+-- when deleting or yanking, text is highlighted
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
   callback = function()
     vim.highlight.on_yank { higroup = "Visual", timeout = 40 }
   end,
 })
 
+-- this autocommand enables spell checking and line wrapping for filetypes in "patter"
 vim.api.nvim_create_autocmd({ "FileType" }, {
   pattern = { "gitcommit", "markdown", "NeogitCommitMessage" },
   callback = function()
     vim.opt_local.wrap = true
     vim.opt_local.spell = true
+  end,
+})
+
+-- this autocommand defines shortcuts for LSP when attaching to a buffer
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    -- this is useful when typing to get the function signature in a floating window
+    vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, opts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    -- this is gonna be done automatically by none-ls formatters when saving
+    -- vim.keymap.set('n', '<space>f', function()
+    --   vim.lsp.buf.format { async = true }
+    -- end, opts)
   end,
 })

0753b013eaf16a373fee0a8ff61dd1aa6ac5215d
Matteo-Tech <matteo.guglilemi.ma.gu@gmail.com>
2024-01-15 14:40:08 +0100

    build(diffview): add layout when calling diffview.

--- lua/user/diffview.lua
+++ lua/user/diffview.lua
@@ -1,9 +1,22 @@
+-- reference : https://github.com/sindrets/diffview.nvim
+
 local M = {
   "sindrets/diffview.nvim",
   event = "VeryLazy",
   cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
 }
 
-function M.config() end
+function M.config()
+  require("diffview").setup {
+    view = {
+      -- default = {},
+      merge_tool = {
+        layout = "diff3_mixed",
+        disable_diagnostics = true,
+        winbar_info = true,
+      }
+    }
+  }
+end
 
 return M

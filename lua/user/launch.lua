3c83d3cf1f4ec50b06e33ce246c2a59ebba77839
Matteo-tech <matteo.guglielmi.ma.gu@gmail.com>
2024-01-15 14:55:53 +0100

    chore: minor comments for info

--- lua/user/launch.lua
+++ lua/user/launch.lua
@@ -1,4 +1,6 @@
 -- utils to create table of specs for lazy loading
+-- this comes from the official GitHub repo (see below)
+-- https://github.com/folke/lazy.nvim#-structuring-your-plugins
 LAZY_PLUGIN_SPEC = {}
 
 function spec(item)
